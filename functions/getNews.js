25191065afa6a7f36b7404a7dd8a88275c2936b7
kanta <kanta.munton@gmail.com>
2021-02-26 00:51:55 +0000

    added cors

--- functions/getNews.js
+++ functions/getNews.js
@@ -2,15 +2,19 @@
 require('dotenv').config();
 const express = require("express")
 const serverless = require("serverless-http")
+const cors = require("cors");
 const NewsAPI = require('newsapi');
 const app = express()
 const bodyParser = require("body-parser")
 const router = express.Router()
 const newsapi = new NewsAPI(process.env.API);
 
+router.use(cors())
+
 app.use(bodyParser.json())
 app.use("/.netlify/functions/getNews", router) // path must route to lambda
 app.use("/", router)
+
 
 router.get("/", (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
