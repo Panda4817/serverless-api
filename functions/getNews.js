ded50dda5cda8f299f32627b4153931df68e172c
kanta <kanta.munton@gmail.com>
2021-02-26 21:19:18 +0000

    fixed bug of unknown country code

--- functions/getNews.js
+++ functions/getNews.js
@@ -23,20 +23,37 @@ router.get("/", (req, res) => {
 
 router.post('/', async (req, res) => {
     const { code } = req.query;
-    try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code.toLowerCase()}&apiKey=${api_key}`);
-        const headlines = response.data.articles.map((item) => {
-            return {
-                'title': item.title,
-                'url': item.url
-            }
-        });
-        res.status(200).json(headlines);
-    } catch (err) {
-        res.status(400).json({ 'error': err.message });
+    const possibleCodes = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id",
+        "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro",
+        "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"
+    ]
+    if (possibleCodes.indexOf(code.toLowerCase()) >= 0) {
+        try {
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code.toLowerCase()}&apiKey=${api_key}`);
+            const headlines = response.data.articles.map((item) => {
+                return {
+                    'title': item.title,
+                    'url': item.url
+                }
+            });
+            res.status(200).json(headlines);
+        } catch (err) {
+            res.status(400).json({ 'error': err.message });
+        }
+    } else {
+        try {
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?language=en&apiKey=${api_key}`);
+            const headlines = response.data.articles.map((item) => {
+                return {
+                    'title': item.title,
+                    'url': item.url
+                }
+            });
+            res.status(200).json(headlines);
+        } catch (err) {
+            res.status(400).json({ 'error': err.message });
+        }
     }
-
-
 })
 
 module.exports = app
