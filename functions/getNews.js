d3f3d0eb2239fe8b36d8df17a52a7380fb4bcd2b
kanta <kanta.munton@gmail.com>
2021-02-26 00:40:22 +0000

    no cors and added headers and redirect to toml file

--- functions/getNews.js
+++ functions/getNews.js
@@ -31,12 +31,6 @@ router.post('/', async (req, res) => {
         })
         res
         .status(200)
-        .header({
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-        })
         .send({ data: headlines })
     } catch (err) {
         console.log(err)
