c72639e03f8d9d00ff2b5d39c2fd6b54899dca83
kanta <kanta.munton@gmail.com>
2021-08-21 18:12:22 +0100

    Updated getNews function with a new api - Newscatcher via Rapid APIs

--- functions/getNews.js
+++ functions/getNews.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const router = express.Router();
 const axios = require("axios");
-const api_key = process.env.API;
+const api_key = process.env.RAPID_API;
 
 router.use(cors());
 
@@ -18,6 +18,35 @@ router.get("/", (req, res) => {
 	res.write("<h1>Up and running</h1>");
 	res.end();
 });
+
+const fetchData = async (code, lang) => {
+	const response = await axios.get(
+		// `https://newsapi.org/v2/top-headlines?country=${code.toLowerCase()}&apiKey=${api_key}`
+		"https://newscatcher.p.rapidapi.com/v1/latest_headlines",
+		{
+			params: {
+				lang: lang,
+				country: code,
+				media: false,
+			},
+			headers: {
+				"x-rapidapi-host": "newscatcher.p.rapidapi.com",
+				"x-rapidapi-key": api_key,
+			},
+		}
+	);
+	return response;
+};
+
+const getHeadlines = (response) => {
+	const headlines = response.data.articles.map((item) => {
+		return {
+			title: item.title,
+			url: item.link,
+		};
+	});
+	return headlines;
+};
 
 router.post("/", async (req, res) => {
 	const { code } = req.query;
@@ -79,30 +108,22 @@ router.post("/", async (req, res) => {
 	];
 	if (possibleCodes.indexOf(code.toLowerCase()) >= 0) {
 		try {
-			const response = await axios.get(
-				`https://newsapi.org/v2/top-headlines?country=${code.toLowerCase()}&apiKey=${api_key}`
-			);
-			const headlines = response.data.articles.map((item) => {
-				return {
-					title: item.title,
-					url: item.url,
-				};
-			});
+			const response = await fetchData(code.toLowerCase(), null);
+			const headlines = getHeadlines(response);
 			res.status(response.status).json(headlines);
 		} catch (err) {
-			res.status(err.response.status).json({ error: err.response.statusText });
+			try {
+				const response = await fetchData(null, "en");
+				const headlines = getHeadlines(response);
+				res.status(response.status).json(headlines);
+			} catch (err) {
+				res.status(err.response.status).json({ error: err.response.statusText });
+			}
 		}
 	} else {
 		try {
-			const response = await axios.get(
-				`https://newsapi.org/v2/top-headlines?language=en&apiKey=${api_key}`
-			);
-			const headlines = response.data.articles.map((item) => {
-				return {
-					title: item.title,
-					url: item.url,
-				};
-			});
+			const response = await fetchData(null, "en");
+			const headlines = getHeadlines(response);
 			res.status(response.status).json(headlines);
 		} catch (err) {
 			res.status(err.response.status).json({ error: err.response.statusText });
